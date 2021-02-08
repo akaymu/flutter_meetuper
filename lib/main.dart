790888bbd7c9b83b1836276a387bff7adc92bb65
Muhlis Murat Akay <akaymu@gmail.com>
2021-02-08 13:51:54 +0300

    Lecture 65

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,38 @@ class MeetuperApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(appTitle),
-        ),
-        body: Center(
-          child: Text(
-            'Welcome in $appTitle!',
-            textDirection: TextDirection.ltr,
-          ),
+      home: CounterHomeScreen(
+        title: appTitle,
+      ),
+    );
+  }
+}
+
+class CounterHomeScreen extends StatelessWidget {
+  final String _title;
+  CounterHomeScreen({String title}) : _title = title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome in $_title, lets increment numbers!',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 15.0),
+            ),
+            Text(
+              'Click Counter: 0',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(fontSize: 30.0),
+            ),
+          ],
         ),
       ),
     );
