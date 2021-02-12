bccccb9c74263845c991d6abaa055b45551d634a
Muhlis Murat Akay <akaymu@gmail.com>
2021-02-12 16:01:10 +0300

    Lecture 105

---
+++ lib/src/services/meetup_api_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/meetup.dart';
+import 'dart:io' show Platform;
+
+class MeetupApiService {
+  final String url = Platform.isIOS
+      ? 'http://localhost:3001/api/v1'
+      : 'http://10.0.2.2:3001/api/v1';
+
+  // Private Constructor
+  MeetupApiService._();
+
+  // Static singleton and factory constructor
+  static final MeetupApiService _singleton = MeetupApiService._();
+  factory MeetupApiService() => _singleton;
+
+  // Fetch Meetups
+  Future<List<Meetup>> fetchMeetups() async {
+    final res = await http.get('$url/meetups');
+
+    final List parsedMeetups = json.decode(res.body);
+    return parsedMeetups.map((val) => Meetup.fromJson(val)).toList();
+  }
+}
