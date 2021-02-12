4fc323e799978eebcb7282300807e6bebf99c9c4
Muhlis Murat Akay <akaymu@gmail.com>
2021-02-12 14:52:11 +0300

    Lecture 103

--- lib/src/screens/meetup_home_screen.dart
+++ lib/src/screens/meetup_home_screen.dart
@@ -10,7 +10,50 @@ class _MeetupHomeScreenState extends State<MeetupHomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
-      body: Center(child: Text('I am Meetup Home Screen')),
+      body: Column(
+        children: <Widget>[
+          MeetupCard(),
+          MeetupCard(),
+          MeetupCard(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class MeetupCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1512136146408-dab5f2ba8ebb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80'),
+            ),
+            title: Text('Meetup in New York'),
+            subtitle: Text('Just some meetup description'),
+          ),
+          ButtonBar(
+            children: <Widget>[
+              FlatButton(
+                child: Text('Visit Meetup'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                onPressed: () {},
+                child: Text('Favorite'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
