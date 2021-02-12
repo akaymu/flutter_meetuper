7bdac55271c053fcb6681a714f1a5d62c3363866
Muhlis Murat Akay <akaymu@gmail.com>
2021-02-12 12:44:12 +0300

    Lecture 101

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  DB_URI:
+    'mongodb+srv://testuser99:testuser99@advancedreact.d8ue0.mongodb.net/test?retryWrites=true&w=majority',
+  SESSION_SECRET: 'muhlismuratakay',
+  JWT_SECRET: 'muhlismuratakay',
+};
