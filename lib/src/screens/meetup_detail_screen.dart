748d6cb49d806277c2b74b98e365c972bb6e715f
Muhlis Murat Akay <akaymu@gmail.com>
2021-02-14 06:43:44 +0300

    Lecture 112

--- lib/src/screens/meetup_detail_screen.dart
+++ lib/src/screens/meetup_detail_screen.dart
@@ -27,25 +27,112 @@ class _MeetupDetailScreenState extends State<MeetupDetailScreen> {
 
   void _fetchMeetupById(String id) async {
     final Meetup meetup = await widget._meetupApiService.fetchMeetupById(id);
-
-    setState(() {
-      this.meetup = meetup;
-    });
+    setState(() => this.meetup = meetup);
   }
 
   @override
   Widget build(BuildContext context) {
-    print(meetup?.id ?? '');
-    print(meetup?.title ?? '');
-    print(meetup?.description ?? '');
     return Scaffold(
       appBar: AppBar(
         title: Text('Meetup Detail'),
       ),
-      body: Center(
-        child: Text(widget.meetupId),
-      ),
+      body: meetup != null
+          ? Column(
+              children: <Widget>[
+                HeaderSection(meetup: meetup),
+                TitleSection(meetup: meetup),
+              ],
+            )
+          : Container(width: 0, height: 0),
       bottomNavigationBar: BottomNavigation(),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  final Meetup meetup;
+  TitleSection({@required this.meetup});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  meetup.title,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  meetup.shortInfo,
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.people,
+            color: Colors.blue[500],
+          ),
+          SizedBox(width: 5.0),
+          Text('${meetup.joinedPeopleCount} People'),
+        ],
+      ),
+    );
+  }
+}
+
+class HeaderSection extends StatelessWidget {
+  final Meetup meetup;
+  HeaderSection({@required this.meetup});
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Stack(
+      alignment: AlignmentDirectional.bottomStart,
+      children: <Widget>[
+        Image.network(
+          meetup.image,
+          width: width,
+          height: 240.0,
+          fit: BoxFit.cover,
+        ),
+        Container(
+          width: width,
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(.3),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30.0,
+              backgroundImage: NetworkImage(
+                  'https://cdn.dribbble.com/users/304574/screenshots/6222816/male-user-placeholder.png?compress=1&resize=400x300'),
+            ),
+            title: Text(
+              meetup.title,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            subtitle: Text(
+              meetup.shortInfo,
+              style: TextStyle(
+                fontSize: 17.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
