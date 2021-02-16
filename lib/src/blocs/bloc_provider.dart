040b18b69e54c6fd23606de37420c0e7220d06d0
Muhlis Murat Akay <akaymu@gmail.com>
2021-02-16 14:03:18 +0300

    Lecture 158

---
+++ lib/src/blocs/bloc_provider.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+// IMPORTANT:
+// This is a GENERIC PROVIDER for all Blocs in our project.
+
+abstract class BlocBase {
+  void dispose();
+}
+
+// T is Bloc Type
+class BlocProvider<T extends BlocBase> extends StatefulWidget {
+  final T bloc;
+  final Widget child;
+
+  BlocProvider({
+    Key key,
+    @required this.child,
+    @required this.bloc,
+  }) : super(key: key);
+
+  @override
+  _BlocProviderState<T> createState() => _BlocProviderState<T>();
+
+  static T of<T extends BlocBase>(BuildContext context) {
+    _BlocProviderInherited<T> provider =
+        context.dependOnInheritedWidgetOfExactType<_BlocProviderInherited<T>>();
+    return provider.bloc;
+  }
+}
+
+class _BlocProviderState<T extends BlocBase> extends State<BlocProvider<T>> {
+  @override
+  void dispose() {
+    widget.bloc?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _BlocProviderInherited<T>(
+      child: widget.child,
+      bloc: widget.bloc,
+    );
+  }
+}
+
+class _BlocProviderInherited<T extends BlocBase> extends InheritedWidget {
+  final T bloc;
+
+  _BlocProviderInherited({
+    Key key,
+    @required Widget child,
+    @required this.bloc,
+  }) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
+}
